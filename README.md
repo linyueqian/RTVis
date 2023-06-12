6268aafc409a747e5a1c0b4c2f69e4ead1b0e239
Yueqian Lin <70319226+linyueqian@users.noreply.github.com>
2023-06-12 16:06:50 +0800

    init

---
+++ README.md
@@ -0,0 +1,17 @@
+# Research-trend-vis
+ 
+### This is a repo for Research Trend Visualization.
+### Authors: Xingyu Shen, Yueqian Lin, Zhixian Zhang
+
+### References:
+
+The codes are modified on the following projects:
+- [1] [Navigation Bar](https://codepen.io/piyushpd139/details/gOYvZPG)
+- [2] [Pop-up Window](https://codepen.io/clarirri/details/ExjEzRo)
+- [3] [Network](https://github.com/tdenzl/MarvelNetwork)
+- [4] [Individual Network](https://github.com/Xovee/canv)
+- [5] [Word Cloud](https://github.com/ecomfe/echarts-wordcloud)
+- [6] [Bar Chart Race](https://github.com/FabDevGit/barchartrace)
+- [7] [Chord Diagram](https://gist.github.com/databayou/c7ac49a23c275f0dd7548669595b8017)
+- [8] [Calendar Heatmap](https://itnext.io/d3-v6-calendar-heat-map-c709fe20e737)
+- [9] [Quadrant Chart](https://github.com/OSTUSA/ScatterPlot)
---
+++ app.py
@@ -0,0 +1,59 @@
+# Import packages
+from dash import Dash, html, dash_table, dcc, callback, Output, Input
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+
+# Incorporate data
+df = pd.read_excel('demo_dataset_new.xlsx')
+
+# Initialize the app
+app = Dash(__name__)
+
+# Create figure
+fig = go.Figure()
+
+# Add trace
+fig.add_trace(
+    go.Scatter(x=list(df['rank']), y=list(df.Counts))
+)
+
+# Set title
+fig.update_layout(
+    title_text="Time series with range slider and selectors"
+)
+
+# App layout
+app.layout = html.Div([
+    html.Div(children='Research Trend Visualization'),
+    html.Hr(),
+    dash_table.DataTable(id='data-table', page_size=6),
+    dcc.Graph(figure=fig, id='controls-and-graph'),
+    dcc.RangeSlider(
+        id='rank-range-slider',
+        value=[df['rank'].min(), df['rank'].max()],
+        marks={str(rank): str(rank) for rank in df['rank'].unique()},
+        step=None
+    )
+])
+
+
+@app.callback(
+    Output(component_id='controls-and-graph', component_property='figure'),
+    Output(component_id='data-table', component_property='data'),
+    Input(component_id='rank-range-slider', component_property='value')
+)
+
+def update_graph(rank_range):
+    filtered_df = df.loc[(df['rank'] >= rank_range[0]) & (df['rank'] <= rank_range[1])]
+    fig = go.Figure()
+    fig.add_trace(
+        go.Scatter(x=list(filtered_df['rank']), y=list(filtered_df.Counts))
+    )
+    filtered_data = filtered_df.to_dict('records')
+    return fig, filtered_data
+
+
+# Run the app
+if __name__ == '__main__':
+    app.run_server(debug=True)
