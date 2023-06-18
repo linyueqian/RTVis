a0f014f1537247a1ddb321664429a6cfac7a3498
Yueqian Lin <70319226+linyueqian@users.noreply.github.com>
2023-06-18 23:28:47 +0800

    update app.py

--- public/app.py
+++ public/app.py
@@ -1,18 +1,22 @@
 import pandas as pd
 import networkx as nx
+import datetime
+import time
 import plotly.graph_objects as go
 import dash
 from dash import Dash, html, dash_table, dcc, callback, Output, Input
 
 # Read the Excel file into a DataFrame
-df = pd.read_excel('../demo_dataset_new.xlsx')
+df = pd.read_excel('demo_dataset.xlsx')
 df['Date'] = pd.to_datetime(df['Date'])
 # sort by df['Date]
 df = df.sort_values(by='Date')
 min_date_ymd = df['Date'].min().to_period('M').to_timestamp().strftime('%Y-%m-%d')
-min_month = datetime.datetime.strptime(min_date_ymd.split('-')[0] + '-' + min_date_ymd.split('-')[1], '%Y-%m')
+min_date_ym = df['Date'].min().to_period('M').to_timestamp().strftime('%Y-%m')
 max_date_ymd = df['Date'].max().to_period('M').to_timestamp().strftime('%Y-%m-%d')
-max_month = datetime.datetime.strptime(max_date_ymd.split('-')[0] + '-' + max_date_ymd.split('-')[1], '%Y-%m')
+max_date_ym = df['Date'].max().to_period('M').to_timestamp().strftime('%Y-%m')
+min_month = df['Date'].dt.to_period('M').min()
+max_month = df['Date'].dt.to_period('M').max()
 ############################################
 # field theme river
 fields_of_struct = []
@@ -32,6 +36,8 @@ df = df[df['field_list'] != 'oratory was in fact a way of establishing selfworth
 ####################
 
 df_field_group = df.groupby('field_list')
+# sort df_field_group alphabetically
+df_field_group = sorted(df_field_group, key=lambda x: x[0], reverse=True)
 field_genres = []
 field_counts = []
 field_months = []
@@ -57,37 +63,39 @@ for field in field_all_traces:
                                          line_shape='spline')
 
 river_fig = go.Figure()
+river_fig.update_layout(plot_bgcolor='white')
+# river_fig.update_xaxes(gridcolor='#F86F03')
+river_fig.update_yaxes(gridcolor='#888')
 for field in field_trace_dict.keys():
     river_fig.add_trace(field_trace_dict[field])
 
 
 
 ###########################################
-# authors
+# authors network
+# Define a function to extract the author names from each entry
+def extract_authors(entry):
+    authors = entry.split(', ')
+    if len(authors) > 1 and authors[-1].endswith(','):
+        # Split the last name and remove the trailing comma
+        last_name = authors[-1][:-1]
+        authors = authors[:-1] + [last_name]
+    authors = [author.strip() for author in authors]
+    return authors
 def generate_node_fig(x_range):
     if x_range is None:
-        df = pd.read_excel('../demo_dataset_new.xlsx')
+        df = pd.read_excel('demo_dataset.xlsx').fillna('')
     else:
-        df = pd.read_excel('../demo_dataset_new.xlsx')
+        df = pd.read_excel('demo_dataset.xlsx').fillna('')
         df = df[(df['Date'] >= x_range[0]) & (df['Date'] <= x_range[1])]
     # Extract the author names from the DataFrame
-    
     author_list = df['Author Name'].tolist()
-
-    # Define a function to extract the author names from each entry
-    def extract_authors(entry):
-        authors = entry.split(', ')
-        if len(authors) > 1 and authors[-1].endswith(','):
-            # Split the last name and remove the trailing comma
-            last_name = authors[-1][:-1]
-            authors = authors[:-1] + [last_name]
-        authors = [author.strip() for author in authors]
-        return authors
     # Create an empty graph
     G = nx.Graph()
 
     # Iterate over each entry in the author_list
-    for entry in author_list:
+    for idx in range(len(author_list)):
+        entry = str(author_list[idx])
         authors = extract_authors(entry)
 
         # Add edges between all pairs of authors in the entry
@@ -116,7 +124,6 @@ def generate_node_fig(x_range):
     G_top = nx.Graph()
     for entry in author_list:
         authors = extract_authors(entry)
-
         # Add edges between all pairs of authors in the entry
         for i in range(len(authors)):
             for j in range(i + 1, len(authors)):
@@ -129,16 +136,20 @@ def generate_node_fig(x_range):
                     else:
                         G_top.add_edge(author1, author2, weight=1)
     # remove the node if it is a single node in the current graph
+    G = G_top.copy()
     for node in G_top.nodes():
-        if len(list(G_top.neighbors(node))) == 0:
-            G_top.remove_node(node)
+        if len(list(G_top.neighbors(node))) <= 1:
+            G.remove_node(node)
 
     # Generate the layout of the graph
-    # pos = nx.kamada_kawai_layout(G_top)
+    pos = nx.kamada_kawai_layout(G, scale=5)
+    # pos = nx.force_atlas2_layout(G, iterations=1000)
+    # pos = forceatlas2.forceatlas2_networkx_layout(G, pos=None, niter=1000)
+    pos = nx.spring_layout(G, pos=pos)
     # pos = nx.spring_layout(G_top)
     # pos = nx.fruchterman_reingold_layout(G_top)
     # pos = nx.spectral_layout(G_top)
-    pos = nx.circular_layout(G_top)
+    # pos = nx.circular_layout(G_top)
 
 
     # Extract node positions and edge coordinates
@@ -153,13 +164,11 @@ def generate_node_fig(x_range):
     node_size = [size / max(node_size) * 100 for size in node_size]
     edge_x = []
     edge_y = []
-    for edge in G_top.edges():
+    for edge in G.edges():
         x0, y0 = pos[edge[0]]
         x1, y1 = pos[edge[1]]
         edge_x.extend([x0, x1, None])
         edge_y.extend([y0, y1, None])
-
-        
 
     # Create edge trace
     edge_trace = go.Scatter(
@@ -178,15 +187,14 @@ def generate_node_fig(x_range):
             color='lightblue',
             opacity=0.9,
             size=node_size,  # Update the marker size based on co-occurrences
-            sizemode='area',
-            sizeref=2. * max(node_size) / (20. ** 2),
+            sizemode='diameter',
         ))
 
     # Create a list of node labels
-    node_labels = list(G_top.nodes())
+    node_labels = list(G.nodes())
 
     # Create node text
-    node_text = [f"{node}<br>Co-occurrences: {co_occurrences[node]}" for node in G_top.nodes()]
+    node_text = [f"{node}<br>Co-occurrences: {co_occurrences[node]}" for node in G.nodes()]
 
     # Update node trace with labels and text
     node_trace.text = node_labels
@@ -205,37 +213,100 @@ def generate_node_fig(x_range):
     return node_fig
 
 node_fig = generate_node_fig(None)
-
-
+##############################################################################################################
+# bar chart race
+def convert_time_to_ordinal(time_str):
+    time_str = time_str.split('.')[0]
+    time_str = time_str.replace('T', ' ')
+    time_str = time_str.replace('Z', '')
+    time_str = time_str.replace(' ', '-')
+    time_str = time_str.replace(':', '-')
+    time_str = time_str.split('-')
+    time_str = [int(x) for x in time_str]
+    time_str = datetime.datetime(time_str[0], time_str[1], time_str[2], time_str[3], time_str[4], time_str[5])
+    return int(time.mktime(time_str.timetuple()))
+def extract_ymd(time_str):
+    time_str = time_str.split('.')[0]
+    time_str = time_str.replace('T', ' ')
+    time_str = time_str.replace('Z', '')
+    time_str = time_str.replace(' ', '-')
+    time_str = time_str.replace(':', '-')
+    time_str = time_str.split('-')
+    time_str = [str(int(x)) for x in time_str]
+    return time_str[0]+'-'+time_str[1]+'-'+time_str[2]
+def generate_race_fig(x_range):
+    if x_range is None:
+        value1 = min_date_ymd
+        value2 = max_date_ymd
+        # print(value1, value2)
+    else:
+        value1, value2 = [extract_ymd(x) for x in x_range]
+    with open('assets/abstractBarChartRace.html', 'r') as file:
+        src_doc = file.read().replace('%value1%', value1).replace('%value2%', value2)
+    return src_doc
+src_doc = generate_race_fig(None)
+##############################################################################################################
 # show the figures using dash
 app = dash.Dash(__name__)
-app.layout = html.Div([
-    dcc.Graph(id='node_fig', figure=node_fig),
-    dcc.Graph(id='river_fig', figure=river_fig),
-])
+app.layout = html.Div(
+    children=[
+        html.Div(
+            className='figure',
+            children=[
+                dcc.Graph(id='node_fig', figure=node_fig)
+            ]
+        ),
+        html.Div(
+            className='figure',
+            children=[
+                html.Iframe(
+                    id='race_fig',
+                    srcDoc=src_doc,
+                    style={
+                        'width': '100%',
+                        'height': '800px',
+                        'border': 'none',
+                        'transform': 'scale(0.7) translateY(-20%)'
+                    }
+                )
+            ]
+        ),
+        html.Div(
+            className='river',
+            children=[
+                dcc.Graph(id='river_fig', figure=river_fig)
+            ]
+        )
+    ]
+)
+
 @app.callback(
     Output('node_fig', 'figure'),
     Output('river_fig', 'figure'),
+    Output('race_fig', 'srcDoc'),
     Input('river_fig', 'relayoutData'))
     
 def update_figure(relayoutData):
     # print(relayoutData)
     if relayoutData is None:
         node_fig = generate_node_fig(None)
-        return node_fig, river_fig
+        return node_fig, river_fig, src_doc
     else:
         if 'xaxis.range[0]' in relayoutData:
             x_range = [relayoutData['xaxis.range[0]'], relayoutData['xaxis.range[1]']]
             river_fig.update_layout(xaxis_range=x_range)
             node_fig = generate_node_fig(x_range)
-            return node_fig, river_fig
+            race_fig = generate_race_fig(x_range)
+            return node_fig, river_fig, race_fig
         elif 'xaxis.autorange' in relayoutData:
             river_fig.update_layout(xaxis_range=None, yaxis_range=None)
             node_fig = generate_node_fig(None)
-            return node_fig, river_fig
+            race_fig = generate_race_fig(None)
+            return node_fig, river_fig, race_fig
         else:
             node_fig = generate_node_fig(None)
-            return node_fig, river_fig
+            race_fig = generate_race_fig(None)
+            return node_fig, river_fig, race_fig
 
 # Run the Dash application
 if __name__ == '__main__':
